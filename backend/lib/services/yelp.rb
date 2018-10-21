5d5429d1bdf309d1b2ce917362fce0b9416748bc
Dan Otto <theDanOtto@gmail.com>
2018-10-21 10:08:29 -0500

    build yelp api service

---
+++ backend/lib/services/yelp.rb
@@ -0,0 +1,30 @@
+class Services::Yelp
+  UnhandledResponseError = Class.new(StandardError)
+
+  def initialize(request_string)
+    @request_string = request_string
+  end
+
+  def fetch_results
+    response = HTTParty.get(@request_string, headers: self.headers)
+
+    case response.code
+
+    when 200..299
+      return response
+    else
+      # IDENTIFY WHERE INVALID RESPONSES ARE HANDLED WHEN FRONTEND AND BACKEND ARE MARRIED
+      puts "ERROR:"
+      puts response
+      raise UnhandledResponseError
+    end
+
+  end
+
+  def headers
+    {
+      "Authorization": "Bearer #{Rails.application.credentials[:yelp][:api_key]}"
+    }
+  end
+
+end
