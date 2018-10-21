bb4f1b51d29cffd7799c0a95a0de50d3276213e2
Dan Otto <theDanOtto@gmail.com>
2018-10-21 12:04:22 -0500

    display results

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Search/>
+    <Results />
   </div>
 </template>
 
 <script>
 import Search from './components/Search';
+import Results from './components/Results';
 
 export default {
   name: 'App',
   components: {
     Search,
+    Results,
   },
 };
 </script>
---
+++ frontend/src/components/Results.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-for="biz in searchData.results.businesses">
+      {{ biz.name }}<br />
+      {{ biz.image_url }}<br />
+      {{ biz.review_count }}<br />
+      {{ biz.rating }}<br />
+      {{ biz.price }}<br />
+      {{ biz.location.display_address }}<br />
+      {{ biz.display_phone }}<br />
+      {{ biz.is_closed }}<br />
+      {{ biz.distance }}
+      <br /><br />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'Results',
+  data() {
+    return {
+    };
+  },
+  computed: {
+    ...mapState([
+      'searchData',
+    ]),
+  },
+  methods: {
+  },
+};
+</script>
+
+<style>
+</style>
