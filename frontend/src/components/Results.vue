e901d6fa1e5cbde757e9f30f9ca1ecd8f1739f87
Dan Otto <theDanOtto@gmail.com>
2018-10-21 18:21:04 -0500

    style results

--- frontend/src/components/Results.vue
+++ frontend/src/components/Results.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <div v-for="biz in alphabetizeBusinesses" :key="biz.id">
-      {{ biz.name }}<br />
-      {{ biz.image_url }}<br />
-      {{ biz.review_count }}<br />
-      {{ biz.rating }}<br />
-      {{ biz.price }}<br />
-      {{ biz.location.display_address.join(", ") }}<br />
-      {{ biz.display_phone }}<br />
-      {{ biz.is_closed }}<br />
-      {{ biz.distance }}
-      <br /><br />
+    <div v-for="biz in alphabetizeBusinesses" :key="biz.id" class="result">
+      <div class="result__main-attributes">
+        <h2 class="result__h2">{{ biz.name }}</h2>
+        <div class="result__reviews">
+          <div class="result__stars" :class="`result__stars--${stripPeriod(biz.rating)}`" style="backgroundImage: url(https://s3-media2.fl.yelpcdn.com/assets/srv0/yelp_design_web/9b34e39ccbeb/assets/img/stars/stars.png);"></div>
+          <span>{{ biz.review_count }} Reviews, {{ biz.price }}</span>
+        </div>
+        <div class="result__contact">
+          <p class="result__p">{{ biz.location.display_address.join(", ") }}</p>
+          <p class="result__p">{{ biz.display_phone }}</p>
+        </div>
+      </div>
+      <hr />
     </div>
   </div>
 </template>
@@ -34,6 +36,9 @@ export default {
     },
   },
   methods: {
+    stripPeriod(rating) {
+      return rating.toString().replace(".", "");
+    }
   },
 };
 </script>
